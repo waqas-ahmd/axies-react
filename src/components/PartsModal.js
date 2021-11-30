5e4f4a21164820b8805e8e512927f485da32b1be
waqas-ahmd <65waqas@gmail.com>
2021-11-30 20:50:10 +0500

    production error resolved

--- src/components/PartsModal.js
+++ src/components/PartsModal.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import genesList from "../assets/genesList";
 import PartCard from "./PartCard";
 import * as styles from "./PartsModal.module.css";
